12a59e98be3033f32be3fedb2b23478605996477
Geoffrey184 <geoffreynono@icloud.com>
2023-06-06 16:16:15 +0200

    tche

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//   jai reussi que js
+$(function () {
+    $('.photo').click (function()  { 
+    
+    $(this).addClass('active').siblings('.photo').removeClass('active');
+
+    $(this).next().show().animate({width:'280px'}).siblings('.description').animate({width: 0}, function() {
+
+    $(this).hide();
+    })});
+ }
+)
