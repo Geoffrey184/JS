2ccf6e3fd1ed6bf64337d392f9e09f594a4007a3
Geoffrey.Noel <126774531+Geoffrey184@users.noreply.github.com>
2023-06-07 00:21:02 +0200

    tche

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 $(function () {
-    $('.photo').click (function()  { 
-    
-    $(this).addClass('active').siblings('.photo').removeClass('active');
+    $('.photo').click(function() {
+        // Vérifie si l'élément cliqué n'a pas déjà la classe "active"
+        if (!$(this).hasClass('active')) {
+            // Supprime la classe "active" de tous les éléments ayant la classe "photo"
+            $('.photo').removeClass('active');
 
-    $(this).next().show().animate({width:'280px'}).siblings('.description').animate({width: 0}, function() {
 
-    $(this).hide();
-    })});
- }
-)
+            // Masque toutes les descriptions
+            $('.description').hide();
+            
+            
+            // Ajoute la classe "active" à l'élément cliqué
+            $(this).addClass('active');
+            
+            
+            // Réinitialise la largeur de toutes les descriptions à 0 pixels
+            $('.description').animate({width: 0});
+            
+           // Affiche la description correspondante à l'élément cliqué
+            $(this).next().show().animate({width:'280px'});
+
+        }
+    });
+});
+//
